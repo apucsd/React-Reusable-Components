888cfab9d362874e2cb48aa43beebfd11fd8c833
Apu Sutra Dhar <apusutradhar77@gmail.com>
2024-01-12 23:27:06 +0600

    tailwindcss custom class

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Container from "./components/ui/Container";
-
 function App() {
   return (
     <>
-      <Container>
-        <button>This is a button</button>
-      </Container>
+      <div className="flex items-center justify-center mt-20">
+        <h1>This is h1</h1>
+        <button className="btn">This is a button</button>
+      </div>
     </>
   );
 }
